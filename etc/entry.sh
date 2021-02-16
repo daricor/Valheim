86ed9747c74303b027f1c2fde6f8dd79b735c11e
Guillaume Vezien <guiv301@hotmail.fr>
2021-02-16 10:44:00 +0100

    Update entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,7 +3,7 @@
 
 deregister_runner() {
     echo "Valheim server is beeing stopped"
-
+    ./vhserver stop
     echo "Valheim server is shutdown"
     exit
 }
@@ -15,7 +15,14 @@ if [ ! -e "/home/linuxgsm/serverfiles" ]; then
 	
 	bash linuxgsm.sh vhserver	
 	echo "Server is installing"
+	./vhserver install
 
 fi
 
+
+echo -e "servername=\"${SERVERNAME}\"\nserverpassword=\"${SERVERPASSWORD}\"\nport=\"${SERVERPORT}\"\ngameworld=\"${GAMEWORLD}\"\npublic=\"${PUBLIC}\"" > /home/linuxgsm/lgsm/config-lgsm/vhserver/common.cfg
+echo "startparameters=\"-name '\${servername}' -password \${serverpassword} -port \${port} -world \${gameworld} -public \${public}\"" >> /home/linuxgsm/lgsm/config-lgsm/vhserver/common.cfg
+
 echo "Server is starting"
+
+./vhserver start
